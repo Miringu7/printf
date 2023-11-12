7b21d1aae5053a4e712cdcbfd08de3aec272a4d1
miringu7 <miringusimeon@gmail.com>
2023-11-12 15:11:29 +0300

    function that produces output according to a format. - not yet completed the function

---
+++ my_printf.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+  *
+  *
+  */
+
+int _printf(const char *format, ...)
+{
+	int num_char_printed = 0;
+	va_list args;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			num_char_printed++;
+		}
+		else
+		{
+			format++;
+			int character = va_arg(args, int);
+
+			if (*format == 'c')
+			{
+				write(1, character, 1);
+			}
+
+			if (*format == 's')
+			{
+				while (*character)
+			}
+
+		}
+		num_char_printed++;
+	}
+	return (num_char);
+}
